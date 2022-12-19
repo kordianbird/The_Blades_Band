fc252856a86036b28063fe52cf040bf575bd2c9d
kordian ptak <kordianbird@Gmail.com>
2022-12-19 23:05:21 +0000

    copy pasted everything

--- js/script.js
+++ js/script.js
@@ -16,3 +16,34 @@ function changeViewPrev() {
         frame.src = currentView + '.html';
     }
 }
+
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+function store() {
+    var name = document.getElementById("demo-hide");
+    window.localStorage.setItem("username", name.value);
+}
+
+window.onload = function displayN() {
+
+    document.getElementById("name").innerHTML =
+        "Hello " + localStorage.getItem("username");
+};
